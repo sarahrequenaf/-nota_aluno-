11e2132ba98c52e562ce4af23a891c602fcaf8e7
sarahrequenaf <128055877+sarahrequenaf@users.noreply.github.com>
2024-08-27 09:37:04 -0300

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,23 @@
+// Passo 1= Criar as constantes que irão armazenar as notas do aluno 
+
+const nota_1 = 10
+const nota_2 = 5
+const nota_3 = 4 
+
+// Passo 2: Calcular a média das notas 
+
+const soma_notas = nota_1 + nota_2 + nota_3 
+const media + soma_notas / 3 
+
+// Passo 3: Verificar se a nota é maior que 6 
+
+if (media >= 6) {
+    console.log('o aluno foi aprovado')
+}
+se media >= 4 && media < 6, console.log(' está de prova final')
+else{
+    console.log('O aluno foi reprovado')
+}
+calcula_nota_alunos(10, 5, 3)
+
+
